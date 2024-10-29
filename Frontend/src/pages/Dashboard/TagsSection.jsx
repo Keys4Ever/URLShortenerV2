ca2e52e9e12f0bd96d5956cc0dec09c512c3d984
Keys4Ever <cubillagonzalo@outlook.com>
2024-10-29 15:58:04 -0300

    Dashboard - Basic Tag fetching

---
+++ Frontend/src/pages/Dashboard/TagsSection.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { Plus, X } from 'lucide-react';
+import Tag from '../../components/Tag';
+
+const TagsSection = ({ tags, isLoading }) => {
+    const [selectedTags, setSelectedTags] = useState([]);
+    console.log(tags);
+    const tagsError = tags.length ? false : true;
+
+    if (isLoading) {
+        return <p className='text-black dark:text-white'>Cargando tags...</p>;
+    }
+
+    return (
+        <div className="border-2 border-white p-4 mb-6">
+            <div className="flex flex-wrap gap-2 mb-4">
+                {tagsError ? (
+                    <p className='text-black dark:text-white'>No tags found</p>
+                ) : (
+                    tags.map(tag => (
+                        <Tag selectedTags={selectedTags} setSelectedTags={setSelectedTags} tag={tag} key={tag.id} />
+                    ))
+                )}
+            </div>
+            <button className="flex items-center gap-2 px-4 py-2 border border-white hover:bg-white hover:text-black transition">
+                <Plus className="w-4 h-4" />
+                Add Tag
+            </button>
+        </div>
+    );
+};
+
+export default TagsSection;
--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -2,12 +2,14 @@ import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../Layout';
 import MainPage from '../pages/MainPage';
+import Dashboard from '../pages/Dashboard';
 
 const AppRoutes = () => {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
                 <Route path="/" element={<Layout><MainPage/></Layout>} />
+                <Route path='/dashboard' element={<Layout><Dashboard /></Layout>} />
             </Routes>
         </Suspense>
     );
