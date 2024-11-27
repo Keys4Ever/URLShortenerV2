0213590a0602a64bbfda210cc472cd77a4ce7bf5
Keys4Ever <cubillagonzalo@outlook.com>
2024-11-27 20:00:32 -0300

    style: sticky nav-bar

--- Frontend/src/components/Nav/Nav.jsx
+++ Frontend/src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import { useAuth } from "../../context/authContext"; 
-import { useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
 import ImageDropMenu from "./components/ImageDropMenu/ImageDropMenu.jsx";
 import LogoCliqueable from './components/LogoCliqueable.jsx';
 import LoginButton from '../LoginButton.jsx';
@@ -11,23 +11,32 @@ const Nav = () => {
     let location = useLocation();
     const [url, setUrl] = useState('');
 
-    useEffect(()=>{
-         setUrl(location.pathname);
-         console.log(url);  
-    },  [location])
+    useEffect(() => {
+        setUrl(location.pathname);
+        console.log(url);
+    }, [location]);
 
     return (
-        <nav className="bg-white dark:bg-black border-b-2 border-black dark:border-white">
+        <nav className="sticky top-0 bg-white dark:bg-black border-b-2 border-black dark:border-white z-50 shadow-sm">
             <div className="flex flex-wrap items-center justify-between max-w-[932px] mx-auto p-4">
                 <LogoCliqueable />
                 <div className="flex items-center gap-4">
-                    {url != '/' ? <ABtn label='Home'link='/' /> : <ABtn link="https://keys4ever.dev" label="Creator Info" className="text-xs sm:text-sm md:text-base lg:text-lg" external />}
+                    {url !== '/' ? (
+                        <ABtn label="Home" link="/" />
+                    ) : (
+                        <ABtn
+                            link="https://keys4ever.dev"
+                            label="Creator Info"
+                            className="text-xs sm:text-sm md:text-base lg:text-lg"
+                            external
+                        />
+                    )}
 
-                    {auth.authenticated ? <ImageDropMenu /> : <LoginButton /> }
+                    {auth.authenticated ? <ImageDropMenu /> : <LoginButton />}
                 </div>
             </div>
         </nav>
     );
-}
+};
 
 export default Nav;
