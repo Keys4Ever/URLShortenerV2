62db650f34b45fa8528cf4d14be93b14e639f617
Keys4Ever <cubillagonzalo@outlook.com>
2024-11-27 17:55:13 -0300

    fix: shortUrl better handling

--- Frontend/src/routes/ShortUrl.jsx
+++ Frontend/src/routes/ShortUrl.jsx
@@ -7,8 +7,20 @@ const ShortUrl = () => {
     const { shortUrl } = useParams();
 
     useEffect(() => {
-        window.location.href = `${BACKEND_URL}/${shortUrl}`;
-    }, []);
+        const fetchUrl = async () => {
+            try {
+                const response = await fetch(`${BACKEND_URL}/api/url/${shortUrl}`, {
+                    method: 'GET',
+                });
+                const data = await response.json();
+                window.location.href = data.originalUrl;
+            } catch (error) {
+                console.error('Error fetching the URL:', error);
+            }
+        };
+
+        fetchUrl();
+    }, [shortUrl]); // Added shortUrl as a dependency
 
     return null;
 };
