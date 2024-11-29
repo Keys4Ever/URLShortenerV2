b34e0e8be27efaf33a828be1406b9d73853a9db4
Keys4Ever <cubillagonzalo@outlook.com>
2024-11-29 19:46:53 -0300

    feat: HeroSection responsive design

--- Frontend/src/components/MainPageComponents/HeroSection.jsx
+++ Frontend/src/components/MainPageComponents/HeroSection.jsx
@@ -9,12 +9,12 @@ const HeroSection = () => {
     return (
         <section className="max-w-5xl mx-auto px-4 py-20 flex items-center">
             <div className="border-4 border-white p-8 text-left w-full mb-6">
-                <h1 className="text-6xl font-bold mb-4 leading-tight">
+                <h1 className="text-xl sm:text-2xl md:text-5xl font-bold mb-4 leading-tight"> 
                     Simple fully functional
                     <br />
-                    URL Shortener
+                    <span className="text-4xl sm:text-3xl md:text-6xl">URL Shortener</span> 
                 </h1>
-                <p className="text-xl mb-6 text-gray-400">
+                <p className="text-lg sm:text-xl mb-6 text-gray-400">
                     Open source project made with React & Node.js
                 </p>
                 
