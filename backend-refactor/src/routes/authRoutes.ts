7b9675cabd0dec26d5768e8789fcaba890e11ddf
Keys4Ever <cubillagonzalo@outlook.com>
2024-12-10 19:55:06 -0300

    feat: Added routes to server.ts

---
+++ backend-refactor/src/routes/authRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { profileController, logoutController, authStatusController, loginController } from '../controllers/authController';
+
+const router = Router();
+
+router.get('/login', loginController);
+router.get('/profile', profileController);
+router.get('/auth-status', authStatusController);
+router.get('/logout', logoutController);
+
+export default router;
--- backend-refactor/src/server.ts
+++ backend-refactor/src/server.ts
@@ -1,6 +1,12 @@
 import express from 'express';
-import {corsConfig} from './config/corsConfig.js';
+import {corsConfig} from './config/corsConfig';
 import cors from 'cors';
+
+import quickRoutes from './routes/quickUrlRoutes';
+import tagsRoutes from './routes/tagsRoutes';
+import statsRoutes from './routes/urlStatRoutes';
+import authRoutes from './routes/authRoutes';
+import urlRoutes from './routes/urlRoutes';
 
 const app = express();
 
@@ -15,6 +21,13 @@ app.get('/', (_req, res) => {
 app.get('/callback', (_req, res) => { 
   res.redirect('http://localhost:5173/');
 });
+
+
+app.use("/api/quick", quickRoutes);
+app.use('/api/tags',  tagsRoutes);
+app.use("/clicks", statsRoutes);
+app.use("/auth", authRoutes);
+app.use("/api/url", urlRoutes);
 
 
 app.listen(3000, () => console.log('Server running on port 3000'));
