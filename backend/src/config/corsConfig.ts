a12f4993f01c8ad394d8bce3e7afd7bd42656862
Keys4Ever <cubillagonzalo@outlook.com>
2024-12-15 21:21:33 -0300

    added origins to cors

--- backend/src/config/corsConfig.ts
+++ backend/src/config/corsConfig.ts
@@ -1,11 +1,13 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
 dotenv.config({ path: '../.env' });
 
-
-
 export const corsConfig = {
-  origin: process.env.FRONTEND_URL,
+  origin: [
+    process.env.FRONTEND_URL,
+    'http://keys.lat',
+    'http://www.keys.lat',
+  ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Authorization', 'Content-Type'],
   credentials: true,
