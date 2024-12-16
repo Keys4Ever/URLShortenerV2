0893b0e9420107cc7c0407232598c77ed844433e
Keys4Ever <cubillagonzalo@outlook.com>
2024-12-15 21:27:45 -0300

    forgot this xd

--- backend/src/config/corsConfig.ts
+++ backend/src/config/corsConfig.ts
@@ -2,12 +2,23 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '../.env' });
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL || '',
+  'https://keys.lat',
+  'https://www.keys.lat',
+];
+
+// Tipo para la función de configuración de CORS
+type CorsOriginCallback = (err: Error | null, allow?: boolean) => void;
+
 export const corsConfig = {
-  origin: [
-    process.env.FRONTEND_URL,
-    'http://keys.lat',
-    'http://www.keys.lat',
-  ],
+  origin: (origin: string | undefined, callback: CorsOriginCallback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`Not allowed by CORS: ${origin}`));
+    }
+  },
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Authorization', 'Content-Type'],
   credentials: true,
