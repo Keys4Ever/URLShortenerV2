beb957b424435d16a1242fc816afb4be4682d4a0
Keys4Ever <cubillagonzalo@outlook.com>
2024-12-16 22:15:52 -0300

    xd

--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../Layout';
+import { Navigate } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import Dashboard from '../pages/Dashboard';
 import ShortUrl from './ShortUrl';
@@ -11,7 +12,7 @@ const AppRoutes = () => {
                 <Route path="/" element={<Layout><MainPage/></Layout>} />
                 <Route path='/dashboard' element={<Layout><Dashboard /></Layout>} />
                 <Route path='/:shortUrl' element={<ShortUrl />} />
-                <Route path='/auth/login' element={window.location.href = "https://keys.lat"} />
+                <Route path='/auth/login' element={<Navigate to="https://keys.lat" replace />} />
             </Routes>
         </Suspense>
     );
